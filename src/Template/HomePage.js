624de2d84c7a03c0e22c4a44cc591bcd119e8378
Markus Wibisono <bloming61@gmail.com>
2022-07-03 21:41:03 +0700

    update
    Signed-off-by: Markus Wibisono <bloming61@gmail.com>

--- src/Template/HomePage.js
+++ src/Template/HomePage.js
@@ -1,6 +1,13 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import '../assets/vendor/aos/aos.css';
+import '../assets/vendor/bootstrap/css/bootstrap.min.css';
+import '../assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import '../assets/vendor/boxicons/css/boxicons.min.css';
+import '../assets/vendor/glightbox/css/glightbox.min.css';
+import '../assets/vendor/swiper/swiper-bundle.min.css';
+import '../assets/css/style.css';
 
 import { Card, Button, Container, Row, Col, Nav, Navbar, NavLink  } from 'react-bootstrap';
 
@@ -18,8 +25,9 @@ function HomePage() {
 
   return (
     <>
-    <section id="topbar" className="d-flex align-items-center">
-        <div className="container d-flex justify-content-center justify-content-md-between">
+
+<section id="topbar" className ="d-flex align-items-center">
+        <div className ="container d-flex justify-content-center justify-content-md-between">
             <div className="contact-info d-flex align-items-center">
             <i className="bi bi-phone d-flex align-items-center ms-4"><span>08881283981</span></i>
           </div>
--- src/Template/NavbarPage.js
+++ src/Template/NavbarPage.js
@@ -2,13 +2,7 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import '../assets/vendor/aos/aos.css';
-import '../assets/vendor/bootstrap/css/bootstrap.min.css';
-import '../assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import '../assets/vendor/boxicons/css/boxicons.min.css';
-import '../assets/vendor/glightbox/css/glightbox.min.css';
-import '../assets/vendor/swiper/swiper-bundle.min.css';
-import '../assets/css/style.css';
+
 
 import { Card, Button, Container, Row, Col, Nav, Navbar, NavLink  } from 'react-bootstrap';
 function NavbarPage() {
